3553e793d93efc13f15e49839be4d0b148b80529
ggr <278830890@qq.com>
2016-10-10 22:04:21 +0800

    删除没用代码

--- Assets/Scripts/GameView.cs
+++ Assets/Scripts/GameView.cs
@@ -5,13 +5,10 @@ public class GameView : MonoBehaviour {
 
     public Camera myCamera;
     public GameObject m_SnakeObjModel;
-    private float time;
-    private Vector3 to;
     // Use this for initialization
     void Start () {
         GameLogin.instance.Init(this);
         GameLogin.instance.m_SelfSnake = GameLogin.instance.CreateSnake("asdfasdfas");
-        to = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
     }
 	
 	// Update is called once per frame
