bda390f837da555b59dee3972da8c1148236e5b3
yyj <407892601@qq.com>
2016-10-10 19:40:14 +0800

    摇杆添加

---
+++ Assets/Rocker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocker : MonoBehaviour {
+    public GameObject obj;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            obj.transform.position = Input.mousePosition;
+            if (Vector3.Distance(obj.transform.localPosition,Vector3.zero) > gameObject.GetComponent<RectTransform>().sizeDelta.x / 2)
+            {
+                obj.transform.localPosition = obj.transform.localPosition.normalized * gameObject.GetComponent<RectTransform>().sizeDelta.x / 2;
+            }
+        }
+    }
+}
