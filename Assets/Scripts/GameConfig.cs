47da343062ea318706e680e5a62affb504353848
--ggr <--278830890@qq.com>
2016-10-20 11:29:41 +0800

    蛇平滑转动

--- Assets/Scripts/GameConfig.cs
+++ Assets/Scripts/GameConfig.cs
@@ -1,6 +1,6 @@
 ﻿
 public class GameConfig
 {
-    public const string serverIP = "127.0.0.1";//"192.168.9.200";
+    public const string serverIP = "192.168.9.201";
     public const int serverPort = 3563;
 }
--- Assets/Scripts/Logic/UI/Rocker.cs
+++ Assets/Scripts/Logic/UI/Rocker.cs
@@ -11,21 +11,27 @@ public class Rocker : UIBase {
         selfRectTrans = transform.GetChild(0).GetComponent<RectTransform>();
         base.OnLoad();
     }
-
+    Vector2 oldPoint = new Vector2(0,0);
+    Vector2 toPoint = Vector2.zero;
     protected override void OnUpdate()
     {
         if (Input.GetMouseButton(0))
         {
             Vector2 point = Vector2.zero;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(selfRectTrans, Input.mousePosition, UIManager.Instance.UICamera, out point);
+            if (Vector2.Angle(oldPoint,point) < 5.0f)
+            {
+                return;
+            }
+            toPoint = point;
             obj.transform.localPosition = new Vector3(point.x, point.y, 0);
-            //obj.transform.position = Input.mousePosition;
             if (Vector3.Distance(obj.transform.localPosition, Vector3.zero) > selfRectTrans.sizeDelta.x / 2)
             {
                 obj.transform.localPosition = obj.transform.localPosition.normalized * selfRectTrans.sizeDelta.x / 2;
             }
-            GameLogin.instance.SetSelfTo(new Vector3(obj.transform.localPosition.x, 0, obj.transform.localPosition.y));
+           
         }
+        AngleTo();
         if (Input.GetMouseButtonUp(0))
         {
             obj.transform.localPosition = Vector3.zero;
@@ -39,5 +45,10 @@ public class Rocker : UIBase {
             GameLogin.instance.SetSelfLength(GameLogin.instance.m_SelfSnake._surplusLength - 1);
         }
         base.OnUpdate();
+    }
+    void AngleTo()
+    {
+        oldPoint = Vector2.Lerp(oldPoint, toPoint, 1.5f * Time.deltaTime);
+        GameLogin.instance.SetSelfTo(new Vector3(oldPoint.x,0, oldPoint.y));
     }
 }
