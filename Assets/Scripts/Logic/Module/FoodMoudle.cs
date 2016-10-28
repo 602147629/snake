660453bd509f21603a432657c92efe092864f3a0
fangzhen <674756872@163.com>
2016-10-28 17:10:17 +0800

    添加食物完成

---
+++ Assets/Scripts/Logic/Module/FoodMoudle.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Snake3D;
+using UnityEditor.VersionControl;
+using Random = UnityEngine.Random;
+[Module("FoodMoudle", true)]
+public class FoodMoudle : ModuleBase
+{
+    private int i = 0;
+    public Dictionary<Int32, FoodItem> mFoodList;
+    // Use this for initialization
+    public override void OnLoad()
+    {
+        NetManager.Instance.AddNetCallback("MsgAddFood", FoodNetInit);
+        NetManager.Instance.AddNetCallback("MsgDelFood", FoodNetDel);
+    }
+
+    public override void OnRelease()
+    {
+        NetManager.Instance.RemoveNetCallback("MsgAddFood", FoodNetInit);
+        NetManager.Instance.RemoveNetCallback("MsgDelFood", FoodNetDel);
+    }
+
+    public void FoodNetInit(object msg)
+    {
+        Debug.Log("+++++++链接成功28--++++++");
+        MsgAddFood initMsg = msg as MsgAddFood;
+        List<MsgFoodStruct> msgConfs = initMsg.FoodList;
+
+       // Debug.Log("+++++++msgConfs--++++++"+ initMsg.FoodList.Count);
+        for (int i = 0; i < msgConfs.Count; i++)
+        {
+            MsgFoodStruct item = msgConfs[i];
+
+            if (mFoodList == null) ;
+            {
+                mFoodList = new Dictionary<Int32, FoodItem>();
+            }
+            FoodItem items = new FoodItem();
+            items.SetId((Int32)item.Id);
+            items.SetPosX((float)item.PosX);
+            items.SetPosY((float)item.PosY);
+            items.SetRadius((float)item.Radius);
+            items.SetScore((Int32)item.Score);
+            mFoodList.Add(items.GetId(), items);
+            Debug.Log("------mFoodList--++++++" + mFoodList.Count);
+            notifyAddFood(items.GetId()+"",items);
+        }
+       // Debug.Log("+++++++链接成功48--++++++");
+        
+    }
+ 
+    public void FoodNetDel(object msg)
+    {
+        Debug.Log("------链接成功--------");
+        List<UInt32> msgConfs = (List<UInt32>)msg ;
+        for (int j = 0; j < msgConfs.Count; j++)
+        {
+            Int32 _deletekey = (Int32) msgConfs[i];
+
+            if (mFoodList.ContainsKey(_deletekey))
+            {
+                mFoodList.Remove(_deletekey);
+                notifyDeleteFood(_deletekey);
+            }
+        }
+
+      
+
+    }
+
+    public void notifyAddFood(String key, FoodItem item)
+    {
+        Notification notify = new Notification("AddFoods", null);
+        notify["Addfoods"] = item;
+        notify["AddFoodsKey"] = key;
+        notify.Send();
+    }
+    public void notifyDeleteFood(Int32 key)
+    {
+        Notification notify = new Notification("DeleteFoods", null);
+        notify["Deletfoods"] = key;
+        notify.Send();
+    }
+}
---
+++ Assets/Scripts/Logic/UI/Foods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Snake3D;
+
+public class Foods : MonoBehaviour
+{
+    public Dictionary<Int32, GameObject> mFoodList;
+    void Start()
+    {
+        if (mFoodList == null)
+        {
+            mFoodList = new Dictionary<int, GameObject>();
+        }
+    }
+
+    void OnEnable()
+    {
+        Messager.Instance.AddNotification("AddFoods", funtionAdd);
+        Messager.Instance.AddNotification("DeleteFoods", funtionDel);
+    }
+
+    void OnDestroy()
+    {
+        Messager.Instance.RemoveNotification("AddFoods",funtionAdd);
+        Messager.Instance.RemoveNotification("DeleteFoods", funtionDel);
+    }
+
+
+    private void funtionAdd(Notification msg)
+    {
+       
+        FoodItem item = msg["Addfoods"] as FoodItem;
+        String  key = msg["AddFoodsKey"].ToString();
+        Vector3 _postion = new Vector3(item.GetPosX(),0,item.GetPosY());
+        string _path = ResConfig.THEME_PATH + UserLogic.Instance.ThemeUsing + "/body";
+        GameObject _bodyRes = Resources.Load<GameObject>(_path);
+        GameObject _food = GameObject.Instantiate(_bodyRes, _postion, Quaternion.identity) as GameObject;
+        _food.name = "Food";
+        _food.transform.parent = transform.GetChild(0);
+        mFoodList.Add(item.GetId(),_food);
+    }
+
+    private void funtionDel(Notification msg)
+    {
+        Int32 deleteKey = (Int32) msg["Deletefoods"];
+        if (mFoodList.ContainsKey(deleteKey))
+        {
+            mFoodList.Remove(deleteKey);
+            Destroy(mFoodList[deleteKey]);
+        }
+       
+    }
+    
+}
